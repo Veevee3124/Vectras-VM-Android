2b37bec78f09a3d453b5ed183b8aa8ee5ad36b14
Veevee3124 <levee684@gmail.com>
2025-05-17 10:27:59 -0700

    sacc

--- app/src/main/java/com/antlersoft/android/bc/BCActivityManagerV5.java
+++ app/src/main/java/com/antlersoft/android/bc/BCActivityManagerV5.java
@@ -4,18 +4,4 @@
 package com.antlersoft.android.bc;
 
 import android.app.ActivityManager;
-
-/**
- * @author Michael A. MacDonald
- */
-public class BCActivityManagerV5 implements IBCActivityManager {
-
-	/* (non-Javadoc)
-	 * @see com.antlersoft.android.bc.IBCActivityManager#getMemoryClass(android.app.ActivityManager)
-	 */
-	@Override
-	public int getMemoryClass(ActivityManager am) {
-		return am.getMemoryClass();
-	}
-
-}
+cfhhcf
--- app/src/main/java/com/vectras/vm/Blog/DataBlog.java
+++ app/src/main/java/com/vectras/vm/Blog/DataBlog.java
@@ -1,9 +1,1 @@
-package com.vectras.vm.Blog;
-
-public class DataBlog {
-
-    public String postThumb;
-    public String postTitle;
-    public String postContent;
-    public String postDate;
-}
+cfhfc
